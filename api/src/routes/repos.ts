a0b52c0247bd993351be8175c9d35a25c0d75460
minhtungo <loducfc@gmail.com>
2022-07-13 11:13:34 -0400

    Fetch Data From The API And Local JSON File
    - Add the Axios library
    - Fetch concurrently data from the sources using Promise.all
    - Filter fetched repos and only return repos whose fork are false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,26 @@
 import { Router, Request, Response } from 'express';
 
+import jsonfile from 'jsonfile';
+import axios from 'axios';
+
 export const repos = Router();
+
+const filePath = './data/repos.json';
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  try {
+    //handle getting data from both sources concurrently and return single aggregated response
+    const response = await Promise.all([
+      jsonfile.readFile(filePath),
+      axios.get('https://api.github.com/users/silverorange/repos'),
+    ]);
+    const fetchedRepo = response.map((repo) => (repo.data ? repo.data : repo));
+    const filteredRepo = fetchedRepo.flat().filter((repo) => !repo.fork);
+    res.status(200).json(filteredRepo);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
